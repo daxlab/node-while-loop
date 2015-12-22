9bf83bacd5cad8035552258fab8d9d9b29860749
daxlab <mandeep25894@gmail.com>
2015-12-22 15:49:20 +0530

    working fine

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by daxlab on 22/12/15.
+ */
+
+var Promise = require('bluebird');
+var pw = {};
+pw.promiseWhile = function (condition, action) {
+    /* condition refers to a bool returning function which when returns false, breaks the loop.
+     action refers to the task which needs to be iteratively performed
+     */
+
+    var resolver = Promise.defer();
+
+    var iter = function () {
+        if (!condition()) return resolver.resolve(action);
+        return Promise.cast(action())
+            .then(iter)
+            .catch(resolver.reject);
+    };
+
+    process.nextTick(iter);
+
+    return resolver.promise;
+};
+
+module.exports = pw;
