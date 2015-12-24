a43a88c0909a45288b76913017630c4a472775e1
daxlab <mandeep25894@gmail.com>
2015-12-24 14:52:26 +0530

    update demo  andbump version to 1.0.4

--- test/demo1.js
+++ test/demo1.js
@@ -5,12 +5,22 @@
 var loop = require('./../index');
 
 var i = 1;
+var j = 1;
 var a = [];
+var b = [];
 loop.while(function () {
     return i * i < 10;
 }, function () {
     a.push(i);
-    console.log(a);
+    console.log('a = ', a);
     i++;
+});
+
+loop.while(function () {
+    return 2 * j < 20;
+}, function () {
+    b.push(j);
+    console.log('b = ', b);
+    j++;
 });
 
